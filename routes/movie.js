c693c0f9ae7db870af25e748d002c4e6241be01c
Enes Ulutaş <enestein07@gmail.com>
2018-09-07 14:54:37 +0300

    Route settings

--- routes/movie.js
+++ routes/movie.js
@@ -12,10 +12,58 @@ router.get('/', (req, res) => {
   });
 });
 
-router.get('/:movie_id', (req, res) => {
-  const promise = MovieModel.findById(req.params.movie_id);
+router.get('/between/:start_year/:finish_year', (req, res) => {
+  const{start_year,finish_year}=req.params;
+  const promise = MovieModel.find({
+    year:{"$gte":parseInt(start_year),"$lte":parseInt(finish_year)} //gte büyük eşit lte küçük eşit e hgarfi olmazsa eşit olmaz
+  });
   promise.then((data) => {
     res.json(data);
+  }).catch((err) => {
+    res.json(err);
+  });
+});
+
+router.get('/topten', (req, res) => {
+  const promise = MovieModel.find({});
+  promise.then((data) => {
+    res.json(data).limit(10).sort({point: -1});
+  }).catch((err) => {
+    res.json(err);
+  });
+});
+
+router.get('/:movie_id', (req, res, next) => {
+  const promise = MovieModel.findById(req.params.movie_id);
+  promise.then((data) => {
+    if (!data)
+      next({ message: 'Fim Bulunamadı' });
+    
+      res.json(data);
+  }).catch((err) => {
+    res.json(err);
+  });
+});
+
+router.delete('/:movie_id', (req, res, next) => {
+  const promise = MovieModel.findOneAndRemove(req.params.movie_id);
+  promise.then((data) => {
+    if (!data)
+      next({ message: 'Fim Bulunamadı' });
+    
+      res.json({status:1});
+  }).catch((err) => {
+    res.json(err);
+  });
+});
+
+router.put('/:movie_id', (req, res, next) => {
+  const promise = MovieModel.findByIdAndUpdate(req.params.movie_id,req.body,{new:true}); //new true güncelleme enasında yeni kaydı getirir
+  promise.then((data) => {
+    if (!data)
+      next({ message: "Fim Bulunamadı" });
+    if(data)
+      res.json(data);
   }).catch((err) => {
     res.json(err);
   });
@@ -33,5 +81,7 @@ router.post('/', function (req, res, next) {
     });
 
 });
+
+
 
 module.exports = router;
